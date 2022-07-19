15d003a218eb195d1d702511b80d86863f55847f
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-07-19 14:48:48 -0300

    upgrade color, and localStorage data

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js'
-
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc,
-  collection,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  deleteField
-} from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-firestore.js'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyA9I8K_x30wUKySQMShAuFPNJ7j695PSdg',
-  authDomain: 'sticky-notes-7c83d.firebaseapp.com',
-  projectId: 'sticky-notes-7c83d',
-  storageBucket: 'sticky-notes-7c83d.appspot.com',
-  messagingSenderId: '143891796037',
-  appId: '1:143891796037:web:5405f2b54620c731c5932c'
-}
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig)
-
-export const db = getFirestore()
-
 //Declaration of variables
 const addBox = document.querySelector('.add-box'),
   modalBox = document.querySelector('.modal-box'),
+  modalTitle = modalBox.querySelector('header p'),
   closeIcon = modalBox.querySelector(' header i'),
   titleTag = modalBox.querySelector(' input '),
   descTag = modalBox.querySelector(' textarea'),
-  addBtn = modalBox.querySelector('button')
+  addBtn = modalBox.querySelector('button'),
+  noteBox = modalBox.querySelector('.note')
 
 const months = [
   'Jan',
@@ -49,14 +23,112 @@ const months = [
   'Dez'
 ]
 
+const colors = [
+  '#E5BEDD',
+  '#FFA8B8',
+  '#DCD9F8',
+  '#F7D8C3',
+  '#FFC8C3',
+  '#BCCEF4',
+  '#B5DCF9',
+  '#A9E5E3',
+  '#A2EDCE',
+  '#A0D995',
+  '#C5D084',
+  '#D2C897',
+  '#FAE187',
+  '#E8BA86',
+  '#D3BEAD'
+]
+
+// getting notes in localstorage
+const notes = JSON.parse(localStorage.getItem('notes') || '[]')
+
+let color = randomColor()
+
+let isUpdate = false,
+  updateId
+
 //Events list
 addBox.addEventListener('click', () => {
+  titleTag.focus()
   modalBox.classList.add('show')
 })
 
 closeIcon.addEventListener('click', () => {
+  isUpdate = false
+  titleTag.value = ''
+  descTag.value = ''
+  addBtn.innerText = 'Adicionar Nota'
+  modalTitle.innerText = 'Nova Nota '
   modalBox.classList.remove('show')
+  showNotes()
 })
+
+// function colors
+function randomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
+// functions notes
+function showNotes() {
+  document.querySelectorAll('.note').forEach(note => note.remove())
+
+  notes.forEach((note, index) => {
+    let liTag = ` <li class="note"              style="background-color: ${randomColor()} ">
+                    <div class="details">
+                    <p>${note.title}</p>
+                    <span
+                      > ${note.description}</span>
+                    </div>
+                    <div class="bottom-content">
+                      <span>${note.date}</span>
+                      <div class="setings">
+                        <i onclick="showMenu(this)" class="bx bx-customize"></i>
+                        <ul class="menu">
+                          <li onclick="updateNote(${index}, '${note.title}', '${
+      note.description
+    }')"><i class='bx bx-edit'></i>Edit</li>
+                          <li onclick="deleteNote(${index})"><i class='bx bx-trash'></i>Delete</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </li>`
+    addBox.insertAdjacentHTML('afterend', liTag)
+  })
+  console.log(randomColor())
+}
+
+showNotes()
+
+function showMenu(elem) {
+  elem.parentElement.classList.add('show')
+  document.addEventListener('click', e => {
+    if (e.target.tagName != 'I' || e.target != elem)
+      elem.parentElement.classList.remove('show')
+  })
+}
+
+function deleteNote(noteId) {
+  let confirmDel = confirm('Você tem certeza que quer deletar está nota?')
+  if (!confirmDel) return
+  notes.splice(noteId, 1)
+  localStorage.setItem('notes', JSON.stringify(notes))
+  showNotes()
+}
+
+// Function edit notes
+
+function updateNote(noteId, title, desc) {
+  isUpdate = true
+  updateId = noteId
+  addBox.click()
+  titleTag.value = title
+  descTag.value = desc
+  addBtn.innerText = 'Concluir'
+  modalTitle.innerText = 'Editar Nota '
+  console.log(noteId, title, desc)
+}
 
 addBtn.addEventListener('click', e => {
   e.preventDefault()
@@ -64,21 +136,6 @@ addBtn.addEventListener('click', e => {
     noteDesc = descTag.value
 
   if (noteTitle || noteDesc) {
-    // async function setNotes() {
-    //   var ref = collection(db, 'notes')
-
-    //   const docRef = await addDoc(ref, {
-    //     title: noteTitle,
-    //     description: noteDesc
-    //   })
-    //     .then(() => {
-    //       alert('Nota Adicionada!')
-    //     })
-    //     .catch(error => {
-    //       alert('Error: ' + error)
-    //     })
-    // }
-
     let dateObj = new Date(),
       month = months[dateObj.getMonth()],
       day = dateObj.getDate(),
@@ -89,10 +146,14 @@ addBtn.addEventListener('click', e => {
       description: noteDesc,
       date: `${day} de ${month}, ${year}`
     }
-
-    const notes = []
-    notes.push(noteInfo)
+    if (!isUpdate) {
+      notes.push(noteInfo) // adding new note to notes
+    } else {
+      isUpdate = false
+      notes[updateId] = noteInfo
+    }
+    //save notes to localstorage
     localStorage.setItem('notes', JSON.stringify(notes))
-    modalBox.classList.remove('show')
+    closeIcon.click()
   }
 })
